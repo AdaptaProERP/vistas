8cb8c65991934a34ee6845e4978c447af32ff604
AdaptaProERP <122864365+AdaptaProERP@users.noreply.github.com>
2025-08-17 09:33:36 -0400

    Add files via upload

---
+++ view_invcosdivmax.sql
@@ -0,0 +1,15 @@
+SELECT       
+MOV_CODSUC                      AS CDY_CODSUC,                    
+MOV_CODCTA                      AS CDY_CODPRO,                        
+MOV_CODIGO                      AS CDY_CODIGO,                        
+MAX(CONCAT(MOV_FECHA,MOV_HORA)) AS CDY_FCHHOR,                
+MAX(MOV_FECHA)                  AS CDY_FECHA,                        
+MOV_COSTO                       AS CDY_COSTO,                         
+MAX(MOV_COSTO/HMN_VALOR)        AS CDY_COSDIV,                        
+(MOV_CANTID * MOV_CONTAB * ( MOV_COSTO * IF(MOV_CONTAB=1, (100-MOV_DESCUE)/100 , 1 ))) AS CDY_TOTAL ,                         
+HMN_VALOR                       AS CDY_VALDIV                        
+FROM DPMOVINV                          
+INNER JOIN DPHISMON ON HMN_CODIGO="USD" AND HMN_FECHA=MOV_FECHA                          
+WHERE MOV_APLORG="C" AND MOV_INVACT=1  AND MOV_CONTAB=1 AND HMN_VALOR>0                        
+GROUP BY MOV_CODSUC,MOV_CODCTA,MOV_CODIGO                        
+ORDER BY MOV_CODSUC,MOV_CODCTA,MOV_CODIGO,MOV_COSTO/HMN_VALOR
---
+++ view_invcosdivmin.sql
@@ -0,0 +1,16 @@
+SELECT     
+MOV_CODSUC                       AS CDX_CODSUC,                      
+ MOV_CODCTA                      AS CDX_CODPRO,                        
+ MOV_CODIGO                      AS CDX_CODIGO,                        
+ MIN(CONCAT(MOV_FECHA,MOV_HORA)) AS CDX_FCHHOR,              
+ MIN(MOV_FECHA)                  AS CDX_FECHA,                        
+ MOV_COSTO                       AS CDX_COSTO,                         
+ MIN(MOV_COSTO/HMN_VALOR)         AS CDX_COSDIV,                        
+(MOV_CANTID * MOV_CONTAB * ( MOV_COSTO * IF(MOV_CONTAB=1, (100-MOV_DESCUE)/100 , 1 ))) AS CDX_TOTAL ,                         
+ HMN_VALOR                       AS CDX_VALDIV                        
+ FROM DPMOVINV                          
+ INNER JOIN DPHISMON ON HMN_CODIGO="USD" AND HMN_FECHA=MOV_FECHA                          
+ WHERE MOV_APLORG="C" AND MOV_INVACT=1  AND MOV_CONTAB=1 AND HMN_VALOR>0                        
+ GROUP BY MOV_CODSUC,MOV_CODCTA,MOV_CODIGO                        
+ ORDER BY MOV_CODSUC,MOV_CODCTA,MOV_CODIGO,MOV_COSTO/HMN_VALOR                   
+ DESC
