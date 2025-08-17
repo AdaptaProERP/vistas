1be970ab1f191f494651c0b54e404104c6fca684
AdaptaProERP <122864365+AdaptaProERP@users.noreply.github.com>
2025-08-17 09:37:35 -0400

    Add files via upload

---
+++ view_docclifavvta.sql
@@ -0,0 +1,18 @@
+SELECT                                                 
+DOC_CODSUC     AS PAG_CODSUC,                                                
+DOC_TIPDOC     AS PAG_TIPDOC,                                                
+DOC_NUMERO     AS PAG_NUMERO,                                                
+DOC_CODIGO     AS PAG_CODIGO,                                                
+DOC_CODVEN     AS PAG_CODVEN,                                            
+DOC_CXC*-1     AS PAG_CXC,                                              
+DOC_TIPTRA     AS PAG_TIPTRA,                                  
+DOC_RECNUM     AS PAG_RECNUM,                                             
+DOC_FECHA      AS PAG_FCHPAG,          
+DOC_NETO       AS PAG_MONTO,      
+DOC_MTOIVA     AS PAG_MTOIVA,  
+DOC_BASNET     AS PAG_BASNET                                                                                    
+FROM  DPDOCCLI                                                
+WHERE (DOC_TIPDOC="FAV" OR DOC_TIPDOC="CRE" OR DOC_TIPDOC="DEV" OR DOC_TIPDOC="CHD")                    
+     AND DOC_TIPTRA="P" AND DOC_ACT=1                                                
+GROUP BY DOC_CODSUC,DOC_TIPDOC,DOC_NUMERO,DOC_RECNUM                                                 
+ORDER BY DOC_CODSUC,DOC_FECHA
---
+++ view_dpmovinvresvta.sql
@@ -0,0 +1,13 @@
+SELECT                   
+MOV_CODSUC AS RVT_CODSUC,                  
+MOV_TIPDOC AS RVT_TIPDOC,                  
+MOV_DOCUME AS RVT_DOCUME,                  
+SUM(MOV_CANTID) AS RVT_CANTID,        
+SUM(MOV_TOTAL)  AS RVT_TOTAL,      
+COUNT(*) AS RTV_COUNT      
+FROM                   
+DPMOVINV                  
+WHERE                  
+MOV_APLORG="V" AND MOV_INVACT=1        
+GROUP BY MOV_CODSUC,MOV_TIPDOC,MOV_DOCUME                 
+ORDER BY MOV_CODSUC,MOV_TIPDOC,MOV_DOCUME
